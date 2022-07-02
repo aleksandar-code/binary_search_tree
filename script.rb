3e2fc8908e2e6c61a2aeb653dc1714d5f1acb8d5
aleksandar-code <aca.jevdjevic@gmail.com>
2022-07-02 14:01:14 +0200

    Create tree class

--- script.rb
+++ script.rb
@@ -1,9 +1,23 @@
-class Node 
-    def initialize(value=nil, left_child=nil, right_child=nil) 
-        @value = value 
-        @left_child = left_child
-        @right_child = right_child
-    end
-    attr_accessor :value, :left_child, :right_child
+# frozen_string_literal: true
 
+# Node class
+class Node
+  def initialize(value = nil, left_child = nil, right_child = nil)
+    @value = value
+    @left_child = left_child
+    @right_child = right_child
+  end
+  attr_accessor :value, :left_child, :right_child
+end
+
+# Tree class
+class Tree
+  def initialize(array = [])
+    @array = array
+    @root = nil
+  end
+
+  def root(root)
+    @root = root
+  end
 end
