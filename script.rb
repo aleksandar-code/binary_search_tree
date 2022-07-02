e44395abef6366a1eab889cc6628686306a8e405
aleksandar-code <aca.jevdjevic@gmail.com>
2022-07-02 14:11:56 +0200

    Create methods for Tree class
    method root and build_tree create

--- script.rb
+++ script.rb
@@ -11,6 +11,13 @@ class Node
 end
 
 # Tree class
+# build_tree method algorithm:
+# 1: Initialize start = 0, end = length of the array – 1
+# 2: mid = (start+end)/2
+# 3: Create a tree node with mid as root (lets call it A).
+# 4: Recursively do following steps:
+# 5: Calculate mid of left subarray and make it root of left subtree of A.
+# 6: Calculate mid of right subarray and make it root of right subtree of A
 class Tree
   def initialize(array = [])
     @array = array
@@ -20,4 +27,13 @@ class Tree
   def root(root)
     @root = root
   end
+
+  def build_tree(array)
+    array.uniq!.sort!
+    array
+  end
 end
+
+my_tree = Tree.new
+
+p my_tree.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
